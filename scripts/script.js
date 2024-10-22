6e99ce0b05a464e9dbc7314d0a0f858b41f6c403
Juan Pablo <jcabanillas@dtransforma.com>
2024-10-22 00:30:14 -0500

    final_JuanPablo

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,25 @@
-const testimonials = document.querySelectorAll('.testimonial'); // todos los testimonios
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-let currentIndex = 0;
-
-function showTestimonial(index) {
-    testimonials.forEach((testimonial, i) => {
-        testimonial.classList.toggle('active', i === index); 
+document.addEventListener("DOMContentLoaded", () => {
+    const prevButton = document.querySelector(".button-prev");
+    const nextButton = document.querySelector(".button-next");
+    const testimonials = document.querySelectorAll(".testimonial-item");
+    let currentIndex = 0;
+  
+    function showTestimonial(index) {
+      testimonials.forEach((testimonial, i) => {
+        testimonial.classList.toggle("active", i === index);
+      });
+    }
+  
+    prevButton.addEventListener("click", () => {
+      currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
+      showTestimonial(currentIndex);
     });
-}
-
-prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length; 
-    showTestimonial(currentIndex); 
-});
-
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % testimonials.length; 
-    showTestimonial(currentIndex); 
-});
-
-showTestimonial(currentIndex);
+  
+    nextButton.addEventListener("click", () => {
+      currentIndex = (currentIndex + 1) % testimonials.length;
+      showTestimonial(currentIndex);
+    });
+  
+    showTestimonial(currentIndex);
+  });
+  
