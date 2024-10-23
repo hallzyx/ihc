b88aa0d8d0574454adbc358df1de3e08d8e2ea46
Juan Pablo <jcabanillas@dtransforma.com>
2024-10-23 18:03:48 -0500

    responsiveV2_Juanpablo

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const prevButton = document.querySelector(".button-prev");
-    const nextButton = document.querySelector(".button-next");
-    const testimonials = document.querySelectorAll(".testimonial-item");
-    let currentIndex = 0;
-  
-    function showTestimonial(index) {
-      testimonials.forEach((testimonial, i) => {
-        testimonial.classList.toggle("active", i === index);
-      });
-    }
-  
-    prevButton.addEventListener("click", () => {
-      currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
-      showTestimonial(currentIndex);
+  const prevButton = document.querySelector(".button-prev");
+  const nextButton = document.querySelector(".button-next");
+  const testimonials = document.querySelectorAll(".testimonial-item");
+  let currentIndex = 0;
+
+  function showTestimonial(index) {
+    testimonials.forEach((testimonial, i) => {
+      testimonial.classList.toggle("active", i === index);
     });
-  
-    nextButton.addEventListener("click", () => {
-      currentIndex = (currentIndex + 1) % testimonials.length;
-      showTestimonial(currentIndex);
-    });
-  
+  }
+
+  prevButton.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
     showTestimonial(currentIndex);
   });
-  
-  const menuToggle = document.getElementById('menu-toggle');
-  const navMenu = document.getElementById('nav-menu');
-  
-  menuToggle.addEventListener('click', () => {
-      navMenu.classList.toggle('show');
+
+  nextButton.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % testimonials.length;
+    showTestimonial(currentIndex);
   });
-  
+
+  showTestimonial(currentIndex);
+});
+
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
